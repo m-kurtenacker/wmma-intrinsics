58b0bf9498db51b9404c1f9a36a6743015d50233
Matthias Kurtenacker <matthias.kurtenacker@dfki.de>
2025-12-03 18:10:47 +0100

    Print XTX results in analysis.py. Updated generator script to generate a lot more combinations.

--- test/generated/generate-cases.py
+++ test/generated/generate-cases.py
@@ -1,22 +1,136 @@
 from itertools import product
 
-sizes = ["16", "32", "64", "128", "256"]
-layouts = ["row", "col"]
-
 def translate(layout):
     if layout == "row":
         return "AddrMode::RowMajor"
     elif layout == "col":
         return "AddrMode::ColMajor"
 
-def generate_test(n, m, k, al, bl, cl):
-    with open("case_%s_%s_%s_%s_%s_%s.art" % (n, m, k, al, bl, cl), "w+") as f:
-        f.write("static N = " + n + ";\n")
-        f.write("static M = " + m + ";\n")
-        f.write("static K = " + k + ";\n")
-        f.write("static alayout = " + translate(al) + ";\n")
-        f.write("static blayout = " + translate(bl) + ";\n")
-        f.write("static clayout = " + translate(cl) + ";\n")
+def generate_test_layout(name, n, l1, l2, l3):
+    with open("%s_labc_%s_%s_%s.art" % (name, l1, l2, l3), "w+") as f:
+        f.write("static N = " + str(n) + ";\n")
+        f.write("static M = " + str(n) + ";\n")
+        f.write("static K = " + str(n) + ";\n")
 
-for args in product(sizes, sizes, sizes, layouts, layouts, layouts):
-    generate_test(*args)
+        f.write("static alayout = " + translate(l1) + ";\n")
+        f.write("static blayout = " + translate(l2) + ";\n")
+        f.write("static clayout = " + translate(l3) + ";\n")
+
+def generate_test_all(name, x, y, xw, yw, pb, l1, l2, l3, kshare, skew):
+    n = 4096
+
+    with open("%s_bxy_%s_%s_wxy_%s_%s_pb_%s_labc_%s_%s_%s_ks_%s_sk_%s.art" % (name, x, y, xw, yw, pb, l1, l2, l3, kshare, skew), "w+") as f:
+        f.write("static N = " + str(n) + ";\n")
+        f.write("static M = " + str(n) + ";\n")
+        f.write("static K = " + str(n) + ";\n")
+
+        f.write("static alayout = " + translate(l1) + ";\n")
+        f.write("static blayout = " + translate(l2) + ";\n")
+        f.write("static clayout = " + translate(l3) + ";\n")
+
+        f.write("static block_x_tiles = " + str(x) + ";\n")
+        f.write("static block_y_tiles = " + str(y) + ";\n")
+
+        f.write("static warp_x_tiles = "  + str(xw) + ";\n")
+        f.write("static warp_y_tiles = "  + str(yw) + ";\n")
+
+        f.write("static k_shared_tiles = " + str(kshare) + ";\n")
+        f.write("static skew_half = " + str(skew) + ";\n")
+
+        f.write("static pb_factor = " + str(pb) + ";\n")
+
+
+layouts = ["row", "col"]
+block_size = [2, 4, 8]
+warp_size = [1, 2, 4]
+k_shared_tiles = [1, 2, 4]
+skew_half = [0, 16]
+pb_factor = [2, 4, 8, 12, 16]
+
+"""
+Used to test blas.
+
+for l1, l2 in product(layouts, layouts):
+    generate_test_layout("test", 256, l1, l2, "col")
+    generate_test_layout("bench", 4096, l1, l2, "col")
+"""
+
+"""
+Used to test the tiled_shmem variant.
+"""
+
+for l2, x, y, xw, yw, kshare, skew in product(layouts, block_size, block_size, warp_size, warp_size, k_shared_tiles, skew_half):
+    l1 = "row"
+    l3 = "row"
+    pb = 0
+
+    if x % xw != 0:
+        continue
+    if y % yw != 0:
+        continue
+
+    if (x // xw) * (y // yw) <= 1: #We need at least two warps for copying.
+        continue
+
+    generate_test_all("bench", x, y, xw, yw, pb, l1, l2, l3, kshare, skew)
+    generate_test_all("test", x, y, xw, yw, pb, l1, l2, l3, kshare, skew)
+
+"""
+Used for testing the wmma variant.
+
+for l2, pb in product(layouts, pb_factor):
+    x = 4
+    y = 4
+    xw = 2
+    yw = 2
+    kshare = 2
+    skew = 16
+    l1 = "row"
+    l3 = "row"
+
+    #if x % xw != 0:
+    #    continue
+    #if y % yw != 0:
+    #    continue
+
+    #if (x // xw) * (y // yw) <= 1: #We need at least two warps for copying.
+    #    continue
+
+    generate_test_all("bench", x, y, xw, yw, pb, l1, l2, l3, kshare, skew)
+    generate_test_all("test", x, y, xw, yw, pb, l1, l2, l3, kshare, skew)
+"""
+
+#sizes = [
+#        64,
+#        128,
+#        256,
+#        512,
+#        512 + 256,
+#        1024,
+#        1024 + 256,
+#        1024 + 512,
+#        1024 + 512 + 256,
+#        2048,
+#        2048 + 512,
+#        2048 + 1024,
+#        2048 + 1024 + 512,
+#        4096,
+#        4096 + 512,
+#        4096 + 1024,
+#        4096 + 1024 + 512,
+#        4096 + 2048,
+#        4096 + 2048 + 512,
+#        4096 + 2048 + 1024,
+#        4096 + 2048 + 1024 + 512,
+#        8192
+#         ]
+
+#def stringfix(n):
+#    k = str(i)
+#    if len(k) == 1:
+#        return "0" + k
+#    else:
+#        return k
+
+#for i in range(0, len(sizes)):
+#    generate_test(stringfix(i), str(sizes[i]))
--- testruns/analysis.py
+++ testruns/analysis.py
@@ -14,26 +14,33 @@ print("Input data size:", df.shape)
 
 
 
-# Compare tiled and shmem results. Row layout no alchemist and col layout on battlemage seem to yield best results for classical tiling w/o shared mem.
+## Compare tiled and shmem results. Row layout no alchemist and col layout on battlemage seem to yield best results for classical tiling w/o shared mem.
 #query1 = df.query('name == "tiled" and ((target == "alchemist" and lb == "row") or (target == "battlemage" and lb == "col"))').assign(ks=-1).groupby(by=["target", "name", "ks", "x", "y", "wx", "wy", "lb"])
 #query2 = df.query('target == "alchemist" and name == "shmem" and sk == 16').groupby(by=["target", "name", "ks", "x", "y", "wx", "wy", "lb"])
-
+#
 #result1 = query1.agg(time=("time", "median")).sort_values("time")
 #result2 = query2.agg(time=("time", "median")).sort_values("time")
 #result = pd.concat([result1, result2]).sort_values("time")
+#
+#print(result)
 
 
 
-# Per device, find best layout for tiled & shared
+## Per device, find best layout for tiled & shared
 #query1 = df.query('target == "alchemist" and name == "tiled"').groupby(by=["target", "name", "x", "y", "wx", "wy", "lb"])
 #query2 = df.query('target == "alchemist" and name == "shmem"').groupby(by=["target", "name", "ks", "sk", "x", "y", "wx", "wy", "lb"])
 #query3 = df.query('target == "battlemage" and name == "tiled"').groupby(by=["target", "name", "x", "y", "wx", "wy", "lb"])
 #query4 = df.query('target == "battlemage" and name == "shmem"').groupby(by=["target", "name", "ks", "sk", "x", "y", "wx", "wy", "lb"])
-
+#
 #result1 = query1.agg(time=("time", "median")).sort_values("time")
 #result2 = query2.agg(time=("time", "median")).sort_values("time")
 #result3 = query3.agg(time=("time", "median")).sort_values("time")
 #result4 = query4.agg(time=("time", "median")).sort_values("time")
+#
+#print(result1)
+#print(result2)
+#print(result3)
+#print(result4)
 
 
 
@@ -67,6 +74,7 @@ result2 = query2.agg(time=("time", "median")).sort_values("time").assign(Tflops=
 result3 = query3.agg(time=("time", "median")).sort_values("time").assign(Tflops=lambda df: (4096 * 4096 * 4096 * 2)/(df.time/1000)/1000000000000)
 result4 = query4.agg(time=("time", "median")).sort_values("time").assign(Tflops=lambda df: (4096 * 4096 * 4096 * 2)/(df.time/1000)/1000000000000)
 
+print()
 print("Best individual runs on battlemage")
 print(result1.head(15))
 print(result2.head(15))
@@ -84,6 +92,7 @@ result1 = query1.agg(time=("time", "median")).sort_values("time").assign(Tflops=
 result2 = query2.agg(time=("time", "median")).sort_values("time").assign(Tflops=lambda df: (4096 * 4096 * 4096 * 2)/(df.time/1000)/1000000000000)
 result3 = query3.agg(time=("time", "median")).sort_values("time").assign(Tflops=lambda df: (4096 * 4096 * 4096 * 2)/(df.time/1000)/1000000000000)
 
+print()
 print("Best individual runs on rtx 2080")
 print(result1.head(15))
 print(result2.head(15))
@@ -92,29 +101,45 @@ print(result3.head(15))
 result_rtx = pd.concat([result1, result2, result3]).sort_values("time")
 
 
-# Compare different versions, all of the same layout.
+query1 = df.query('target == "7900-xtx" and name == "tiled"').assign(ks=-1, sk=-1, pb=-1).groupby(by=["target", "name", "pb", "ks", "sk", "x", "y", "wx", "wy", "lb"])
+query2 = df.query('target == "7900-xtx" and name == "blocked"').groupby(by=["target", "name", "pb", "ks", "sk", "x", "y", "wx", "wy", "lb"])
+query3 = df.query('target == "7900-xtx" and name == "wmma"').assign(x=-1, y=-1, wx=-1, wy=-1, ks=-1, sk=-1).groupby(by=["target", "name", "pb", "ks", "sk", "x", "y", "wx", "wy", "lb"])
+
+result1 = query1.agg(time=("time", "median")).sort_values("time").assign(Tflops=lambda df: (4096 * 4096 * 4096 * 2)/(df.time/1000)/1000000000000)
+result2 = query2.agg(time=("time", "median")).sort_values("time").assign(Tflops=lambda df: (4096 * 4096 * 4096 * 2)/(df.time/1000)/1000000000000)
+result3 = query3.agg(time=("time", "median")).sort_values("time").assign(Tflops=lambda df: (4096 * 4096 * 4096 * 2)/(df.time/1000)/1000000000000)
+
+print()
+print("Best individual runs on 7900 xtx")
+print(result1.head(15))
+print(result2.head(15))
+print(result3.head(15))
+
+result_xtx = pd.concat([result1, result2, result3]).sort_values("time")
+
+
+print()
+print("Best runs overall")
+print(result_alchemist.head(15))
+print(result_battlemage.head(15))
+print(result_rtx.head(15))
+print(result_xtx.head(15))
+
+
+## Compare different versions, all of the same layout.
 #query1 = df.query('target == "alchemist" and name == "tiled"').assign(sk=0, ks=1).groupby(by=["name", "pb", "x", "y", "wx", "wy", "lb"])
 #query2 = df.query('target == "alchemist" and name != "tiled" and sk == 0 and ks == 1').groupby(by=["name", "pb", "x", "y", "wx", "wy", "lb"])
-
+#
 #result1 = query1.agg(time=("time", "median"))
 #result2 = query2.agg(time=("time", "median"))
 #result = pd.concat([result1, result2]).groupby(by=["x", "y", "wx", "wy", "lb"], group_keys=False)
-
-
-#To figure out what target/name combinations are available.
-#result = df.groupby(by=["target", "name"]).agg(time=("time", "median"))
-
-
+#
 #for r in result:
 #    print(r)
 
-#print(result)
-#print(result1.head(15))
-#print(result2.head(15))
 
-#print("Best runs overall")
-#print(result_alchemist.head(15))
-#print(result_battlemage.head(15))
-#print(result_rtx.head(15))
+##To figure out what target/name combinations are available.
+#result = df.groupby(by=["target", "name"]).agg(time=("time", "median"))
+#print(result)
 
 #embed()
